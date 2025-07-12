fb6a79b0d7d6f46550e8fd322320d941fddad29d
YenNhi205-hash <nhipnyps38368@gmail.com>
2025-07-12 12:32:19 +0700

    Update DiscoutBlog, ChatPage, Home, EmojiPopperMultiPositionProps

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import UserPage from './pages/User.jsx';
 import { PaymentPage } from './pages/Payment';
 import { AccountPage } from './pages/AccountPage.jsx';
 import DiscountBlog from './pages/DiscountBlog.jsx';
+import ChatPage from './pages/ChatPage.jsx';
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       <Route path="/payment" element={<PaymentPage />} />
       <Route path="/account" element={<AccountPage />} />
       <Route path="/discounts" element={<DiscountBlog />} /> {/* Thêm route cho DiscountBlog */}
+      <Route path="/chat" element={<ChatPage />} />
     </Routes>
   );
 }
